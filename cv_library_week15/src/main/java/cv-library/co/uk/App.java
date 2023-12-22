01e55db71b3dde39df87345e2b7a99b337f6e0d5
Jignashathummar <jignashakotadiya10@gmail.com>
2023-12-22 11:02:55 +0000

    cv-library wednesday

---
+++ cv_library_week15/src/main/java/cv-library/co/uk/App.java
@@ -0,0 +1,13 @@
+package cv-library.co.uk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
